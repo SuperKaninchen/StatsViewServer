0bba2b546bd9964f8bc2d7dbc1eb9a24af303602
Max Nijenhuis <max.nijenhuis@perfact.de>
2023-08-11 12:59:45 +0200

    make settings form togglable

--- statsviewer_SuperKaninchen/static/script.js
+++ statsviewer_SuperKaninchen/static/script.js
@@ -1,5 +1,17 @@
+const settings_form = document.forms["settings-form"]
 const submitSettingsForm = () => {
-    document.forms["settings-form"].submit()
+    settings_form.submit()
+}
+const toggleSettingsForm = (heading) => {
+    if (settings_form.style.display == "none") {
+        settings_form.style.display = "block";
+        heading.innerHTML = "Settings (hide)"
+    } else {
+        settings_form.style.display = "none";
+        heading.innerHTML = "Settings (show)"
+    }
+    settings_form.style.visibility = !settings_form.style.visibility
+    console.log("toggling")
 }
 
 const timeframe_select = document.getElementById("timeframe-select")
@@ -7,3 +19,6 @@ timeframe_select.addEventListener("change", submitSettingsForm)
 
 const view_select = document.getElementById("view-select")
 view_select.addEventListener("change", submitSettingsForm)
+
+const settings_heading = document.getElementById("settings-heading")
+settings_heading.addEventListener("click", function () {toggleSettingsForm(settings_heading)})
